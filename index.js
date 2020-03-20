b9d165b536166a8718079a2959b1bb029c230e58
Geoluread <62258867+Geoluread-dev@users.noreply.github.com>
2020-03-20 10:17:11 +0100

    Minor changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
-const { token } = require("./config.json");
+require('dotenv').config();
 require("./functions")(bot);
 
 bot.handlers = new Discord.Collection();
@@ -11,4 +11,4 @@ module.exports = {
 	bot: bot
 };
 
-bot.login(token);
+bot.login(process.env.TOKEN);
