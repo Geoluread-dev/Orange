966c819ff63e2d178ee1505638ee5bcfff07932c
Geoluread <62258867+Geoluread-dev@users.noreply.github.com>
2020-03-20 19:29:13 +0100

    Fixed permissions

--- commands/foo.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: "foobar",
-  description: "",
-  permission: ["Moderators", "Kneusjes", "Yeet", "bar", "foo"],
-  execute(Discord, bot, message, args){
-    message.channel.send("bar");
-  }
-}
