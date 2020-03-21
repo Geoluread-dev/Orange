1e925f11e7aea601d2911deb45a6fb3503f3e41c
Geoluread <62258867+Geoluread-dev@users.noreply.github.com>
2020-03-21 15:49:51 +0100

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -3,22 +3,19 @@ module.exports = {
   description: "",
   permission: ["684373684764278806"],
   execute(Discord, bot, message, args) {
-    if(!message.channel.id === "690247061571895319") return message.channel.send("Command is not send to the right channel");
-    if(!args[1]) return message.channel.send("Please give the poll a title");
-    let msgArgs = args.slice(0).join(" ")
+    if(!args[0]) return message.channel.send("Please give the poll a title");
+    let msgTitle = args.slice(0).join(" ");
+    let msgArgs = args.slice(1).join(" ");
 
     var embed = new Discord.MessageEmbed()
       .setAuthor(message.author.username, message.author.displayAvatar())
       .setColor("#ff5733")
+      .setTitle(msgTitle)
       .setDescription(msgArgs);
 
-    message.channel.send(embed)
-
-    bot.channels.fetch('689929818795409543')
-      .then(channel => channel.send(embed)
-      .then(messageReaction => {
-        messageReaction.react("👍🏿");
-        messageReaction.react("👎🏿");
-      })).catch(console.error);
+    message.channel.send(embed).then(messageReaction => {
+      messageReaction.react("👍🏿");
+      messageReaction.react("👎🏿");
+    })).catch(console.error);
   }
 }
